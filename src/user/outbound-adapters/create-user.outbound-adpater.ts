9a1e77756409e4a571610dc029b588b9052bf412
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-04-24 00:27:30 +0900

    FIX: (User) UserRepository 주입

--- src/user/outbound-adapters/create-user.outbound-adpater.ts
+++ src/user/outbound-adapters/create-user.outbound-adpater.ts
@@ -4,13 +4,28 @@ import {
   CreateUserOutboundPort,
 } from '../oubound-port/create-user.outbound-port';
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CreateUserOutboundAdapter implements CreateUserOutboundPort {
-  execute(
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async execute(
     param: CreateUserOutboundInputDto,
   ): Promise<CreateUserOutboundOutputDto> {
     console.log(param);
+
+    const userCreation = await this.userRepository.create(param);
+
+    // TODO try-catch 추가
+    const result = await this.userRepository.save(userCreation);
+
+    // TODO output 제대로 추가
     return Promise.resolve(new CreateUserOutboundOutputDto());
   }
 }
