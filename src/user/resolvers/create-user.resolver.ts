153487c82ad6185637b7df84f4720e7f2e7c24b2
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-04-23 17:15:18 +0900

    FIX: (User) 임시로 만들어둔 getUser 쿼리 삭제

--- src/user/resolvers/create-user.resolver.ts
+++ src/user/resolvers/create-user.resolver.ts
@@ -20,12 +20,4 @@ export class CreateUserResolver {
   ): Promise<CreateUserInboundPortOutputDto> {
     return await this.createUserService.execute(createUserInputDto);
   }
-
-  // TODO: 실제 필요한 query 생성하고 나면 삭제 필요(최소 1개 이상의 query가 있어야 autoSchema가 정상 동작해서 임시로 넣어둠)
-  @Query(() => CreateUserInboundPortOutputDto)
-  async getUser(
-    @Args('input') createUserInputDto: CreateUserInboundPortInputDto,
-  ) {
-    return null;
-  }
 }
