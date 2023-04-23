1d2f7a4c624014a6cc5357a2371281c8cd45df0e
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-04-23 17:43:00 +0900

    FIX: (User) CreateUser 서비스에서 Outbound 주입해서 사용

--- src/user/services/create-user.service.ts
+++ src/user/services/create-user.service.ts
@@ -3,14 +3,23 @@ import {
   CreateUserInboundPortInputDto,
   CreateUserInboundPortOutputDto,
 } from '../inbound-ports/create-user.inbound-port';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  CREATE_USER_OUTBOUND_PORT,
+  CreateUserOutboundPort,
+} from '../oubound-port/create-user.outbound-port';
 
 @Injectable()
 export class CreateUserService implements CreateUserInboundPort {
+  constructor(
+    @Inject(CREATE_USER_OUTBOUND_PORT)
+    private readonly createUserOutboundAdapter: CreateUserOutboundPort,
+  ) {}
+
   async execute(
     params: CreateUserInboundPortInputDto,
   ): Promise<CreateUserInboundPortOutputDto> {
     console.log(params);
-    return Promise.resolve(new CreateUserInboundPortOutputDto());
+    return await this.createUserOutboundAdapter.execute(params);
   }
 }
