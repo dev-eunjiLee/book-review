3028007e9fdfad9181e90aa479800c26953fee5d
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-04-23 17:15:39 +0900

    FIX: (User) ReadUserResolver 등록

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CREATE_USER_INBOUND_PORT } from './inbound-ports/create-user.inbound-port';
 import { CreateUserService } from './services/create-user.service';
 import { CreateUserResolver } from './resolvers/create-user.resolver';
+import { ReadUserResolver } from './resolvers/read-user.resolver';
 
 @Module({
   providers: [
@@ -13,6 +14,7 @@ import { CreateUserResolver } from './resolvers/create-user.resolver';
 
     // resolvers
     CreateUserResolver,
+    ReadUserResolver,
   ],
 })
 export class UserModule {}
