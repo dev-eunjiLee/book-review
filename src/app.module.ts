3d95cdb210aca0e1094a64fc6ec25bafdafe667e
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-04-23 16:26:40 +0900

    FIX: (App) authSchemaFile: true

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,8 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
     // * util
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      // code first 방식을 사용할 예정이기 때문에 자동으로 스키마 파일을 생성해서 등록할 수 있도록 옵션 설정
+      autoSchemaFile: true,
     }),
     // * service
     UserModule,
