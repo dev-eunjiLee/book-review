52b0fd74773391d95aab2432bec207cf8318a46c
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-04-23 16:24:39 +0900

    FIX: (User) register CreateUserService, CreateUserResolver

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
+import { CREATE_USER_INBOUND_PORT } from './inbound-ports/create-user.inbound-port';
+import { CreateUserService } from './services/create-user.service';
+import { CreateUserResolver } from './resolvers/create-user.resolver';
 
 @Module({
-  providers: [],
+  providers: [
+    // services implemented by interfaces
+    {
+      provide: CREATE_USER_INBOUND_PORT,
+      useClass: CreateUserService,
+    },
+
+    // resolvers
+    CreateUserResolver,
+  ],
 })
 export class UserModule {}
