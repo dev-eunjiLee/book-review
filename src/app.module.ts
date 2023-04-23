2e590b383b7cf71d977466be07bf9128867cf6ef
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-04-23 23:22:55 +0900

    FIX: (App) ConfigModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -13,6 +14,10 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
       // code first 방식을 사용할 예정이기 때문에 자동으로 스키마 파일을 생성해서 등록할 수 있도록 옵션 설정
       autoSchemaFile: true,
       includeStacktraceInErrorResponses: false,
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env.dev',
+      isGlobal: true,
     }),
     // * service
     UserModule,
