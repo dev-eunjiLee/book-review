daca08730ab7394fc6e610621b0032a9ba564cc0
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-04-25 00:09:38 +0900

    FIX: (Common) CommonEntity의 속성들을 gql에도 등록

--- src/common/common.entity.ts
+++ src/common/common.entity.ts
@@ -4,17 +4,23 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Field, ObjectType } from '@nestjs/graphql';
 
+@ObjectType()
 export class CommonEntity {
+  @Field(() => Date)
   @CreateDateColumn()
   createdAt: Date;
 
+  @Field(() => Date)
   @UpdateDateColumn()
   updatedAt: Date;
 
+  @Field(() => Number)
   @PrimaryGeneratedColumn()
   tableId: number;
 
+  @Field(() => Date)
   @DeleteDateColumn()
   deletedAt: Date;
 }
