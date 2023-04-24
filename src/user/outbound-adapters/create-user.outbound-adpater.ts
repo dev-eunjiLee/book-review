384cfb19e9c5bb0a00a7b0d6fca4370f7dc3d539
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-04-25 00:10:51 +0900

    FIX: (User) 중복값이 있는 경우 graphqlError 반환

--- src/user/outbound-adapters/create-user.outbound-adpater.ts
+++ src/user/outbound-adapters/create-user.outbound-adpater.ts
@@ -4,9 +4,10 @@ import {
   CreateUserOutboundPort,
 } from '../oubound-port/create-user.outbound-port';
 import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { QueryFailedError, Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { GraphQLError } from 'graphql/error';
 
 @Injectable()
 export class CreateUserOutboundAdapter implements CreateUserOutboundPort {
@@ -22,10 +23,21 @@ export class CreateUserOutboundAdapter implements CreateUserOutboundPort {
 
     const userCreation = await this.userRepository.create(param);
 
-    // TODO try-catch 추가
-    const result = await this.userRepository.save(userCreation);
-
+    let user: User;
+    try {
+      user = await this.userRepository.save<User>(userCreation);
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        if ('code' in error) {
+          const code = error.code;
+          if (code === 'ER_DUP_ENTRY') {
+            throw new GraphQLError('Duplication');
+          }
+        }
+      }
+      throw new GraphQLError(error);
+    }
     // TODO output 제대로 추가
-    return Promise.resolve(new CreateUserOutboundOutputDto());
+    return { ok: true };
   }
 }
