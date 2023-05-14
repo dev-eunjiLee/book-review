9c600f688a46eb49d3b1143cbee96e1a05e0c2f2
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-05-15 00:19:21 +0900

    FIX: (App) 로그 인터셉터 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './util/logger/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: false }));
+
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   await app.listen(3000);
 }
