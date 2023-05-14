14df7d8d5e35af1c7df03a61fa2de25e69aff4ba
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-05-14 21:45:48 +0900

    FIX: (로그) request 처음 들어왔을 때 로그 완성

--- src/util/logger/logPlugin.ts
+++ src/util/logger/logPlugin.ts
@@ -4,22 +4,16 @@
 // * ===== ===== * //
 
 export const myPlugin = {
-  // Fires whenever a GraphQL request is received from a client.
   async requestDidStart(requestContext: any) {
-    console.log('Request started! Query:\n' + requestContext.request.query);
-
-    return {
-      // Fires whenever Apollo Server will parse a GraphQL
-      // request to create its associated document AST.
-      async parsingDidStart(requestContext: any) {
-        console.log('Parsing started!');
+    const {
+      request: {
+        http: { method, headers },
       },
+    } = requestContext;
 
-      // Fires whenever Apollo Server will validate a
-      // request's document AST against your GraphQL schema.
-      async validationDidStart(requestContext: any) {
-        console.log('Validation started!');
-      },
-    };
+    const host = headers.get('host');
+    const query = requestContext.request.query;
+
+    console.log(`${method}, ${host}, ${query}`);
   },
 };
