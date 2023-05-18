429b3e72dea66926fd67dd1a4f8e2b0358df6471
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-05-19 00:03:50 +0900

    FIX: input에 requestId 추가

--- src/user/inbound-ports/create-user.inbound-port.ts
+++ src/user/inbound-ports/create-user.inbound-port.ts
@@ -1,13 +1,20 @@
-import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
+import {
+  Field,
+  InputType,
+  IntersectionType,
+  ObjectType,
+  PickType,
+} from '@nestjs/graphql';
 import { CommonOutputDto } from '../../common/common.output.dto';
 import { User } from '../entities/user.entity';
+import { CommonDto } from '../../common/common.dto';
 
 export const CREATE_USER_INBOUND_PORT = 'CREATE_USER_INBOUND_PORT';
 
 @InputType()
-export class CreateUserInboundPortInputDto extends PickType<User, keyof User>(
-  User,
-  ['id', 'nickname', 'password'],
+export class CreateUserInboundPortInputDto extends IntersectionType(
+  PickType<User, keyof User>(User, ['id', 'nickname', 'password']),
+  CommonDto,
 ) {}
 
 // TODO: CreateUserInboundPortOutputDto 완성
