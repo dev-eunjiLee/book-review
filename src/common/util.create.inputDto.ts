84beffa0f797f0bd56ffacfd91ce9bce83cd55e4
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-05-19 00:03:35 +0900

    FIX: 사용하지 않는 파일 삭제

--- src/common/util.create.inputDto.ts
+++
@@ -1,5 +0,0 @@
-import { CommonInputDto } from './common.input.dto';
-
-// TODO 클래스 2개를 병합하는 mixedIn 적용 필요
-// https://radlohead.gitbook.io/typescript-deep-dive/type-system/mixins
-// requestId를 모든 input에 넣어주기 위해
