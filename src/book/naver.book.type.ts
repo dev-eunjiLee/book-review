39ff44f4417dd9ee26b73cb15ac6b6317dec096d
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-05-21 22:16:29 +0900

    FIX: 네이버 도서 호출 관련 에러 관련 타입, 상수 셋팅

---
+++ src/book/naver.book.type.ts
@@ -0,0 +1,17 @@
+//NAVER_BOOK_SEARCH_ERROR_CODE
+export const NAVER_BOOK_SEARCH_ERROR_CODE_LIST = [
+  'SE01',
+  'SE02',
+  'SE03',
+  'SE04',
+  'SE05',
+  'SE06',
+  'SE99',
+] as const;
+type NAVER_BOOK_SEARCH_ERROR_CODE_LIST_TYPE =
+  typeof NAVER_BOOK_SEARCH_ERROR_CODE_LIST[number];
+
+export type NAVER_BOOK_SEARCH_ERROR_BODY = {
+  errorMessage: string;
+  errorCode: NAVER_BOOK_SEARCH_ERROR_CODE_LIST_TYPE;
+};
