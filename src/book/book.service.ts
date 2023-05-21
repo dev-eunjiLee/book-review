a14f9bb4b75e0ebdcb0e6326ff8964925242119d
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-05-21 21:34:49 +0900

    FIX: 네이버 API 이용해서 책 검색해오기

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import got from 'got';
 import { ConfigService } from '@nestjs/config';
+import { BOOK_FROM_NAVER_TYPE } from './book.entity';
+import {
+  ReadBookListInputDto,
+  ReadBookListOutputDto,
+} from './dtos/read.book.list.dto';
 
 @Injectable()
 export class BookService {
   constructor(private readonly configService: ConfigService) {}
-  async readBookList() {
+  async readBookList(
+    param: ReadBookListInputDto,
+  ): Promise<ReadBookListOutputDto> {
     const basicUrl = `https://openapi.naver.com/v1/search/book.json`;
 
-    const url = `${basicUrl}?query=성경`;
+    const { keyword, pageNumber, pageSize } = param;
 
-    const t = await got(url, {
+    const url = `${basicUrl}?query=${keyword}&start=${pageNumber}&display=${pageSize}`;
+
+    const result = (await got(url, {
       headers: {
         'X-Naver-Client-Id': this.configService.get<string>('NAVER_CLIENT_ID'),
         'X-Naver-Client-Secret': this.configService.get<string>(
           'NAVER_CLIENT_SECRET',
         ),
       },
-    }).json();
+    }).json()) as BOOK_FROM_NAVER_TYPE;
 
-    console.log(t);
-
-    return 'ok';
+    return {
+      ...result,
+      bookList: result.items,
+    };
   }
 }
