7c463c520f2b034d3a5cbcdcd999845d58eaffc4
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-05-28 01:52:52 +0900

    FIX: (REVIEW) 로직 순서용 주석 추가, 책 가져오기 호출

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { BookService } from 'src/book/book.service';
 
 @Injectable()
 export class ReviewService {
+  constructor(private readonly bookService: BookService) {}
   createReview() {
+    // * 1. 유저가 선택한 책이 book DB에 있는지 확인
+    const t = this.bookService.readBook();
+
+    // * 2. bookDB에 없는 경우 DB에 저장
+
+    // * 3. bookDB에서 책을 선택해서 연결한 후 리뷰 작성
     return 'ok';
   }
 }
