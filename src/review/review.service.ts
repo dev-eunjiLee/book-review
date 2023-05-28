7f5d045ab64668af849f21504111d94b675aba72
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-05-28 12:03:02 +0900

    FIX: (REVIEW) 책 선택 로직 추가, 책 없는 경우 분기처리까지만 셋팅

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { BookService } from 'src/book/book.service';
+import { CreateReviewInputDto } from './dtos/create.review.dto';
 
 @Injectable()
 export class ReviewService {
   constructor(private readonly bookService: BookService) {}
-  createReview() {
+  createReview(param: CreateReviewInputDto) {
     // * 1. 유저가 선택한 책이 book DB에 있는지 확인
-    const t = this.bookService.readBook();
+    const pickedBook = this.bookService.readBook({ title: param.title });
 
     // * 2. bookDB에 없는 경우 DB에 저장
+    if (pickedBook === undefined) {
+      // TODO 책 검색 시작
+    }
 
     // * 3. bookDB에서 책을 선택해서 연결한 후 리뷰 작성
     return 'ok';
