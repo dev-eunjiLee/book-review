470775d97d8c829767f64d3e3b1e54cfe5a1a7ad
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-05-29 23:11:36 +0900

    FIX: (BOOK) entity 칼럼셋팅

--- src/book/types/entities/book.entity.ts
+++ src/book/types/entities/book.entity.ts
@@ -1,27 +1,44 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { CommonEntity } from 'src/common/common.entity';
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 @InputType('BookInputType')
 @ObjectType()
 @Entity('book')
 export class Book extends CommonEntity {
+  @Column()
   @Field(() => String)
   title: string; // 책 제목
+
+  @Column()
   @Field(() => String)
   link: string; // 네이버 도서 정보 URL
+
+  @Column()
   @Field(() => String)
   image: string; // 썸네일 이미지의 URL
+
+  @Column()
   @Field(() => String)
   author: string; // 저자 이름
+
+  @Column()
   @Field(() => String)
   discount: number; // 판매 가격. 절판 등의 이유로 가격이 없으면 값을 반환하지 않음
+
+  @Column()
   @Field(() => String)
   publisher: string; // 출판사 이름
+
+  @Column()
   @Field(() => String)
   isbn: number; // ISBN
+
+  @Column()
   @Field(() => String)
   description: string; // 네이버 도서의 책 소개
+
+  @Column()
   @Field(() => String)
   pubdate: Date; // 출간일
 }
